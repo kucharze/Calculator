de431e5882debd5958929095ad4237f3e4d7286e
Zachary Kuchar <zacharyk323@live.com>
2023-07-05 13:56:31 -0400

    Set up calculations and fixed issues

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const calculateTerms = (x, y, sign) => {
   if (sign === "+") {
     display.innerHTML = parseInt(x) + parseInt(y);
   } else if (sign === "-") {
-    display.innerHTML = parseInt(x) + parseInt(y);
+    display.innerHTML = parseInt(x) - parseInt(y);
   } else if (sign === "x") {
     display.innerHTML = parseInt(x) * parseInt(y);
   } else if (sign === "%") {
@@ -29,11 +29,24 @@ const calculateTerms = (x, y, sign) => {
 const splitScreen = () => {
   let input = display.textContent; //The current text on the screen
   console.log(input);
+  clearScreen();
 
   if (input.includes("+")) {
+    let plus = input.split("+");
+    console.log("plus", plus);
+    calculateTerms(plus[0], plus[1], "+");
   } else if (input.includes("-")) {
+    let minus = input.split("-");
+    console.log("minus", minus);
+    calculateTerms(minus[0], minus[1], "-");
   } else if (input.includes("x")) {
+    let times = input.split("x");
+    console.log("multiply", times);
+    calculateTerms(times[0], times[1], "x");
   } else if (input.includes("%")) {
     //divide
+    let divide = input.split("%");
+    console.log("Divide", divide);
+    calculateTerms(divide[0], divide[1], "%");
   }
 };
